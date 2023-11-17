26c47b0842f320f05908f1af4c87c322b4ffb04f
Arslan <arslanjs.dev@gmail.com>
2023-11-17 15:34:21 +0500

    Add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,12 @@
+import { Heading } from "@chakra-ui/react";
+import Layout from "./Layout";
+
 function Footer() {
-    return <></>
+  return (
+    <Layout bg="black" color="white">
+      <Heading textAlign="center" fontSize="xl">All rights are reserved by @BakeryStore</Heading>
+    </Layout>
+  );
 }
 
 export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,29 @@
-import { HStack, Box, Text, Image } from "@chakra-ui/react";
+import { HStack, Box, Text, Image, Link } from "@chakra-ui/react";
 
 const Navbar = () => {
-  return <Box w="full">
-    <HStack justifyContent="space-between" alignContent="center" pb="0" px="10" bg="white" borderBottom="1px solid" borderColor="gray.100">
+  return (
+    <Box w="full">
+      <HStack
+        justifyContent="space-between"
+        alignContent="center"
+        pb="0"
+        px="10"
+        bg="white"
+        borderBottom="1px solid"
+        borderColor="gray.100"
+      >
         <Box>
-            <Image src="images/logo.png" w="75px" />
+          <Image src="images/logo.png" w="75px" />
         </Box>
         <HStack gap="50px">
-            <a href="#">Home</a>
-            <a href="#">Menu</a>
-            <a href="#">About Us</a>
-            <a href="#">Contact Us</a>
+          <Link href="/">Home</Link>
+          <Link href="/">Menu</Link>
+          <Link href="/">About Us</Link>
+          <Link href="/">Contact Us</Link>
         </HStack>
-    </HStack>
-  </Box>;
+      </HStack>
+    </Box>
+  );
 };
 
 export default Navbar;
