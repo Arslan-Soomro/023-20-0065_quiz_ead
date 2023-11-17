337fb7c35837b5f9702e34afcdabd5821c526ffe
Arslan <arslanjs.dev@gmail.com>
2023-11-17 15:30:45 +0500

    Add Contact Us

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,4 +1,25 @@
-export const  ContactUs = () => {
-    return <></>
-};
+import { Input, Textarea, VStack, HStack, Heading, Button } from "@chakra-ui/react";
+import Layout from "./Layout";
 
+export const ContactUs = () => {
+  return (
+    <Layout bg="white" color="black">
+
+      <VStack justifyContent="flex-start" alignItems="flex-start" gap="30px">
+        <Heading>Contact Us</Heading>
+        <HStack w="100%" gap="50px">
+          <Input placeholder="Full Name"  />
+          <Input placeholder="Phone" />
+        </HStack>
+        <HStack w="100%" gap="50px">
+          <Input placeholder="Address" />
+          <Input placeholder="Email" />
+        </HStack>
+        <HStack w="100%">
+          <Textarea placeholder="Message" resize="none" minH="200px" />
+        </HStack>
+        <Button alignSelf="flex-end">Submit</Button>
+      </VStack> 
+    </Layout>
+  );
+};
